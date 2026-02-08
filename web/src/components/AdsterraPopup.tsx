6b7705eb9de75ea56e60318f89b489362e16f95d
albinchristo04 <priyanka0101111@gmail.com>
2026-02-08 18:05:45 +0530

    22

--- web/src/components/AdsterraPopup.tsx
+++ web/src/components/AdsterraPopup.tsx
@@ -2,27 +2,18 @@ import { useEffect } from 'react';
 
 export function AdsterraPopup() {
     useEffect(() => {
-        // Load Adsterra Popup Ad
-        const adsterraScript = document.createElement('script');
-        adsterraScript.src = 'https://pl28674382.effectivegatecpm.com/70/61/73/706173a2f090622107c8ff833a0914ef.js';
-        adsterraScript.async = true;
+        // Create script element
+        const script = document.createElement('script');
+        script.src = 'https://pl28674382.effectivegatecpm.com/70/61/73/706173a2f090622107c8ff833a0914ef.js';
+        script.async = true;
         
-        // Load AddThis CPA Ad
-        const addthisScript = document.createElement('script');
-        addthisScript.src = 'https://qkzpkdm.com/cu/f61797eca7ec2e1d.js?force_https=1';
-        addthisScript.async = true;
+        // Add script to document head
+        document.head.appendChild(script);
         
-        // Add scripts to document head
-        document.head.appendChild(adsterraScript);
-        document.head.appendChild(addthisScript);
-        
-        // Cleanup function to remove scripts when component unmounts
+        // Cleanup function to remove script when component unmounts
         return () => {
-            if (document.head.contains(adsterraScript)) {
-                document.head.removeChild(adsterraScript);
-            }
-            if (document.head.contains(addthisScript)) {
-                document.head.removeChild(addthisScript);
+            if (document.head.contains(script)) {
+                document.head.removeChild(script);
             }
         };
     }, []);
